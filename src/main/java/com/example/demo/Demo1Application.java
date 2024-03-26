57698c24886ca33230c412c0e7f66dc8c82b82c5
lihhso <144242919+lihhso@users.noreply.github.com>
2024-03-26 19:48:05 +0800

    三层为分

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.Mapper.UserMapper;
-import com.example.demo.pojo.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,12 +1,6 @@
 package com.example.demo;
 
-import com.example.demo.Mapper.UserMapper;
-import com.example.demo.pojo.User;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest//SpringBOOt整合单元测试的注解
 class Demo1ApplicationTests {
